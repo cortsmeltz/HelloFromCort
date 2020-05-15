86005b92793afc9048f0dcf991f8ee98a4e9360c
Brennan Smeltzer <sme18001@byui.edu>
2020-05-15 15:50:02 -0600

    Added a line of code saying who edited this file.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Cort");
+        System.out.println("Hello Cort, my name is Brennan");
 
     }
 }
